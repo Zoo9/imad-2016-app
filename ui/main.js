32684345cf7d6fabe33a15af1eb32e377d824c42
Zoo9 <aneeskardar@gmail.com>
2016-09-25 10:00:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //change the text
 var element = document.getElementById('main-text');
 element.innerHTML = 'Bye bye';
+//move the image
+var img = getElementById('madi');
+img.onclick = function(){
+    img.style.marginleft = '100px';
+};
