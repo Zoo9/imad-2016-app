8b0a0dac024e5062db9037d23d9149461cdac85e
Zoo9 <aneeskardar@gmail.com>
2016-09-26 21:34:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'Bye bye';
 //move the image
 var img = document.getElementById('madi');
+var marginRight = '10px';
 img.onclick = function(){
-    img.style.marginLeft = '100px';
+    moveRight = marginRight + '10px';
 };
