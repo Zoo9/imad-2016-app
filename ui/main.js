20f4a7988ef086af0199e55a53a5889b438486e4
Zoo9 <aneeskardar@gmail.com>
2016-09-28 19:52:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,26 @@
+//counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function() {
+  //Make a request to the counter endpoint
+  
+  //Capture the response and store it in a variable
+  
+  //render the variable in a correct span
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML  = counter.toString();
+};
+
+
+
+
+
+
+
+
+
+/*
 console.log('Loaded!');
 //change the text
 var element = document.getElementById('main-text');
@@ -12,3 +35,4 @@ function moveRight(){
 img.onclick = function(){
     var interval = setInterval(moveRight, 100);
 };
+*/
